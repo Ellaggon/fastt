9e37ff3adf55f395c6b649af3ff585dba5f87b36
ellaggon <ellaggon@tutamail.com>
2025-10-13 18:23:39 -0300

    feat(iconMaps): add icon maps

---
+++ src/lib/utils/iconMaps.ts
@@ -0,0 +1,10 @@
+import { Wifi, Coffee, Car, Tent, Waves, Languages } from "lucide-react"
+
+export const iconMap: Record<string, any> = {
+	wifi: Wifi,
+	coffee: Coffee,
+	car: Car,
+	tent: Tent,
+	waves: Waves,
+	languages: Languages,
+}
