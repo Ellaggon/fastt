105d4d84366e193b5c8cb90829fe977ffe510c73
ellaggon <ellaggon@tutamail.com>
2025-11-17 17:51:31 -0300

    refactor(productFormSchema): update validation rules for name and description fields, and make base price fields optional

--- src/schemas/productFormSchema.ts
+++ src/schemas/productFormSchema.ts
@@ -2,24 +2,34 @@ import { z } from "zod"
 
 export const productFormSchema = z.object({
 	providerId: z.string().min(1, "providerId requerido"),
-	name: z.string().min(3, "Mínimo 3 caracteres").max(120, "Máximo 120"),
+	name: z
+		.string()
+		.min(10, "El título debe tener al menos 10 caracteres")
+		.max(50, "El título no debe superar los 50 caracteres."),
 	productType: z.enum(["Hotel", "Tour", "Package"], {
 		required_error: "Tipo de producto requerido",
 	}),
-	description: z.string().max(5000, "Máx 5000").optional().or(z.literal("")),
+	description: z
+		.string()
+		.min(10, "La descripcion debe tener al menos 10 caracteres")
+		.max(200, "La descripcion no debe superar los 200 caracteres")
+		.optional()
+		.or(z.literal("")),
 	destinationId: z.string().min(1, "Selecciona una ciudad o localidad"),
-	basePriceUSD: z.coerce.number().min(0, "Precio >= 0"),
-	basePriceBOB: z.coerce.number().min(0, "Precio >= 0"),
+	basePriceUSD: z.coerce.number().optional(),
+	basePriceBOB: z.coerce.number().optional(),
 	// imágenes: validamos en cliente con la metadata y en server por seguridad
 	imagesMeta: z
 		.array(
 			z.object({
 				name: z.string(),
-				size: z.number().max(10 * 1024 * 1024, "Imagen <= 10MB"), // ajusta a tu límite real
+				size: z
+					.number()
+					.max(10 * 1024 * 1024, "El tamaño de cada imagen no debe ser superior a los 10 KB."), // ajusta a tu límite real
 				type: z.string().regex(/^image\//, "Debe ser imagen"),
 			})
 		)
-		.min(1, "Sube al menos 1 imagen"),
+		.min(1, "Debe subir al menos una imagen."),
 })
 
 export type ProductForm = z.infer<typeof productFormSchema>
