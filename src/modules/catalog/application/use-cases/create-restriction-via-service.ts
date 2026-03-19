bafed7e3b09892f571bec55b283be9716b3c4b3d
ellaggon <ellaggon@tutamail.com>
2026-03-18 21:09:48 -0300

    chore(catalog): remove legacy restriction service use-cases
    - Delete create-restriction-via-service.ts: superseded by direct use-case
    - Delete preview-restrictions-via-service.ts: functionality moved to preview endpoint
    - Simplify restriction management by consolidating use-cases

--- src/modules/catalog/application/use-cases/create-restriction-via-service.ts
+++
@@ -1,6 +0,0 @@
-import { restrictionService } from "@/container"
-
-export async function createRestrictionViaService(body: any): Promise<Response> {
-	await restrictionService.create(body)
-	return new Response("ok")
-}
