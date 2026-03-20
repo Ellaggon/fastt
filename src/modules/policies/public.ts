5adf3c50d619541f6a92b08bc55e5cd4f4b8608a
ellaggon <ellaggon@tutamail.com>
2026-03-19 23:09:44 -0300

    feat(policies): add module public interface
    - Export public API for policies module
    - Define exported use-cases and services
    - Enable external module consumption
    - Complete policies module interface

---
+++ src/modules/policies/public.ts
@@ -0,0 +1,41 @@
+// Public API for the policies module.
+// External consumers MUST import from "@/modules/policies/public".
+// NOTE: Infrastructure exports exist only to support composition-root wiring (container).
+
+// Domain
+export * from "./domain/policy.priority"
+export * from "./domain/cancellation/cancellationEngine"
+export * from "./domain/restrictions/RestrictionRuleEngine"
+export * from "./domain/restrictions/restrictions.conflicts"
+export * from "./domain/restrictions/restrictions.guards"
+export * from "./domain/restrictions/restrictions.params"
+export * from "./domain/restrictions/restrictions.priority"
+export * from "./domain/restrictions/restrictions.types"
+
+// Application mappers
+export * from "./application/mappers/restrictions.mapper"
+
+// Application services
+export * from "./application/services/RestrictionService"
+
+// Application ports
+export * from "./application/ports/EffectivePolicyRepositoryPort"
+export * from "./application/ports/PolicyCachePort"
+export * from "./application/ports/PolicyCommandRepositoryPort"
+export * from "./application/ports/PolicyQueryRepositoryPort"
+
+// Application use-cases
+export * from "./application/use-cases/activate-policy"
+export * from "./application/use-cases/apply-policy-preset"
+export * from "./application/use-cases/assign-policy-group"
+export * from "./application/use-cases/build-policy-snapshot"
+export * from "./application/use-cases/create-policy"
+export * from "./application/use-cases/create-policy-version"
+export * from "./application/use-cases/delete-draft-policy"
+export * from "./application/use-cases/get-policy"
+export * from "./application/use-cases/list-assigned-policies"
+export * from "./application/use-cases/list-policy-history"
+export * from "./application/use-cases/resolve-policies"
+export * from "./application/use-cases/resolve-policy-by-hierarchy"
+export * from "./application/use-cases/run-policy-compiler"
+export * from "./application/use-cases/unassign-policy-group"
