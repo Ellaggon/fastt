aac0c9cbad5d809de3b386b9c93a2f979dff668e
ellaggon <ellaggon@tutamail.com>
2026-03-26 18:40:13 -0300

    refactor(container-inventory): update inventory dependency wiring

--- src/container/inventory.container.ts
+++ src/container/inventory.container.ts
@@ -1,13 +1,17 @@
 import { AvailabilityService, InventorySeederService } from "@/modules/inventory/public"
 
 import { DailyInventoryRepository } from "../modules/inventory/infrastructure/repositories/DailyInventoryRepository"
+import { InventoryHoldRepository } from "../modules/inventory/infrastructure/repositories/InventoryHoldRepository"
 import { InventoryRepository } from "../modules/inventory/infrastructure/repositories/InventoryRepository"
+import { VariantInventoryConfigRepository } from "../modules/inventory/infrastructure/repositories/VariantInventoryConfigRepository"
 import { InventoryBootstrapper } from "../modules/inventory/infrastructure/services/InventoryBootstrapper"
 import { RecomputeInventoryService } from "../modules/inventory/infrastructure/services/RecomputeInventoryService"
 
 // ---- Infrastructure singletons ----
 export const dailyInventoryRepository = new DailyInventoryRepository()
+export const inventoryHoldRepository = new InventoryHoldRepository()
 export const inventoryRepository = new InventoryRepository()
+export const variantInventoryConfigRepository = new VariantInventoryConfigRepository()
 export const inventoryBootstrapper = new InventoryBootstrapper()
 export const recomputeInventoryService = new RecomputeInventoryService()
 
