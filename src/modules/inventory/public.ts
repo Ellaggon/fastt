ae138997880bdc0a57ee9b0ca43ceb0ce1e8acf2
ellaggon <ellaggon@tutamail.com>
2026-03-26 18:55:44 -0300

    refactor(inventory-application): align ports and public exports

--- src/modules/inventory/public.ts
+++ src/modules/inventory/public.ts
@@ -8,6 +8,9 @@ export * from "./domain/InventoryReservationService"
 // Application use-cases
 export { canReserveInventory } from "./application/use-cases/can-reserve-inventory"
 export { recomputeInventory } from "./application/use-cases/recompute-inventory"
+export { holdInventory } from "./application/use-cases/hold-inventory"
+export { releaseInventoryHold } from "./application/use-cases/release-inventory-hold"
+export { releaseExpiredHolds } from "./application/use-cases/release-expired-holds"
 export type { DailyInventorySnapshot } from "./application/use-cases/can-reserve-inventory"
 
 // Application services
@@ -18,3 +21,5 @@ export * from "./application/services/InventorySeederService"
 export * from "./application/ports/DailyInventoryRepositoryPort"
 export * from "./application/ports/InventoryBootstrapPort"
 export * from "./application/ports/InventoryRepositoryPort"
+export * from "./application/ports/VariantInventoryConfigRepositoryPort"
+export * from "./application/ports/InventoryHoldRepositoryPort"
